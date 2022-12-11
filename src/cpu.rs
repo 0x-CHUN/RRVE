d5bb9fe9d45eb64f86bcffb36775da2707b2ab2f
0x-CHUN <chunhui.xie@qq.com>
2022-12-11 20:45:12 +0800

    complete bus and memory

---
+++ src/cpu.rs
@@ -0,0 +1,469 @@
+use crate::bus::Bus;
+use crate::exception::Exception;
+use crate::param::{DRAM_BASE, DRAM_END};
+
+// The `CPU` struct that contains registers, a program counter, system bus that connects
+pub struct CPU {
+    // 32 64-bit integer registers.
+    pub regs: [u64; 32],
+    // pc register
+    pub pc: u64,
+    // System bus
+    pub bus: Bus,
+}
+
+const RVABI: [&str; 32] = [
+    "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
+];
+
+impl CPU {
+    pub fn new(code: Vec<u8>) -> Self {
+        let mut regs = [0; 32];
+        regs[2] = DRAM_END;
+        let pc = DRAM_BASE;
+        let bus = Bus::new(code);
+        Self {
+            regs,
+            pc,
+            bus,
+        }
+    }
+
+    pub fn reg(&self, r: &str) -> u64 {
+        match RVABI.iter().position(|&x| x == r) {
+            Some(i) => self.regs[i],
+            None => match r {
+                "pc" => self.pc,
+                "fp" => self.reg("s0"),
+                r if r.starts_with("x") => {
+                    if let Ok(i) = r[1..].parse::<usize>() {
+                        if i <= 31 { return self.regs[i]; }
+                        panic!("Invalid register {}", r);
+                    }
+                    panic!("Invalid register {}", r);
+                }
+                _ => panic!("Invalid register {}", r),
+            }
+        }
+    }
+
+    /// Load a value from a dram.
+    pub fn load(&mut self, addr: u64, size: u64) -> Result<u64, Exception> {
+        self.bus.load(addr, size)
+    }
+
+    /// Store a value to a dram.
+    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), Exception> {
+        self.bus.store(addr, size, value)
+    }
+
+    /// Get an instruction from the dram.
+    pub fn fetch(&mut self) -> Result<u64, Exception> {
+        self.bus.load(self.pc, 32)
+    }
+
+    /// Update pc register pc + 4
+    #[inline]
+    pub fn update_pc(&mut self) -> Result<u64, Exception> {
+        return Ok(self.pc + 4);
+    }
+
+    /// Execute an instruction after decoding. Return true if an error happens, otherwise false.
+    pub fn execute(&mut self, inst: u64) -> Result<u64, Exception> {
+        let opcode = inst & 0x0000007f;
+        let rd = ((inst & 0x00000f80) >> 7) as usize;
+        let rs1 = ((inst & 0x000f8000) >> 15) as usize;
+        let rs2 = ((inst & 0x01f00000) >> 20) as usize;
+        let funct3 = (inst & 0x00007000) >> 12;
+        let funct7 = (inst & 0xfe000000) >> 25;
+
+        // Emulate that register x0 is hardwired with all bits equal to 0.
+        self.regs[0] = 0;
+
+        match opcode {
+            0x03 => {
+                // imm[11:0] = inst[31:20]
+                let imm = ((inst as i32 as i64) >> 20) as u64;
+                let addr = self.regs[rs1].wrapping_add(imm);
+                match funct3 {
+                    0x0 => {
+                        // lb
+                        let val = self.load(addr, 8)?;
+                        self.regs[rd] = val as i8 as i64 as u64;
+                        return self.update_pc();
+                    }
+                    0x1 => {
+                        // lh
+                        let val = self.load(addr, 16)?;
+                        self.regs[rd] = val as i16 as i64 as u64;
+                        return self.update_pc();
+                    }
+                    0x2 => {
+                        // lw
+                        let val = self.load(addr, 32)?;
+                        self.regs[rd] = val as i32 as i64 as u64;
+                        return self.update_pc();
+                    }
+                    0x3 => {
+                        // ld
+                        let val = self.load(addr, 64)?;
+                        self.regs[rd] = val;
+                        return self.update_pc();
+                    }
+                    0x4 => {
+                        // lbu
+                        let val = self.load(addr, 8)?;
+                        self.regs[rd] = val;
+                        return self.update_pc();
+                    }
+                    0x5 => {
+                        // lhu
+                        let val = self.load(addr, 16)?;
+                        self.regs[rd] = val;
+                        return self.update_pc();
+                    }
+                    0x6 => {
+                        // lwu
+                        let val = self.load(addr, 32)?;
+                        self.regs[rd] = val;
+                        return self.update_pc();
+                    }
+                    _ => Err(Exception::IllegalInstruction(inst)),
+                }
+            }
+            0x13 => {
+                // imm[11:0] = inst[31:20]
+                let imm = ((inst & 0xfff00000) as i32 as i64 >> 20) as u64;
+                // "The shift amount is encoded in the lower 6 bits of the I-immediate field for RV64I."
+                let shamt = (imm & 0x3f) as u32;
+                match funct3 {
+                    0x0 => {
+                        // addi
+                        self.regs[rd] = self.regs[rs1].wrapping_add(imm);
+                        return self.update_pc();
+                    }
+                    0x1 => {
+                        // slli
+                        self.regs[rd] = self.regs[rs1] << shamt;
+                        return self.update_pc();
+                    }
+                    0x2 => {
+                        // slti
+                        self.regs[rd] = if (self.regs[rs1] as i64) < (imm as i64) { 1 } else { 0 };
+                        return self.update_pc();
+                    }
+                    0x3 => {
+                        // sltiu
+                        self.regs[rd] = if self.regs[rs1] < imm { 1 } else { 0 };
+                        return self.update_pc();
+                    }
+                    0x4 => {
+                        // xori
+                        self.regs[rd] = self.regs[rs1] ^ imm;
+                        return self.update_pc();
+                    }
+                    0x5 => {
+                        match funct7 >> 1 {
+                            // srli
+                            0x00 => {
+                                self.regs[rd] = self.regs[rs1].wrapping_shr(shamt);
+                                return self.update_pc();
+                            }
+                            // srai
+                            0x10 => {
+                                self.regs[rd] = (self.regs[rs1] as i64).wrapping_shr(shamt) as u64;
+                                return self.update_pc();
+                            }
+                            _ => Err(Exception::IllegalInstruction(inst)),
+                        }
+                    }
+                    0x6 => {
+                        self.regs[rd] = self.regs[rs1] | imm;
+                        return self.update_pc();
+                    } // ori
+                    0x7 => {
+                        self.regs[rd] = self.regs[rs1] & imm; // andi
+                        return self.update_pc();
+                    }
+                    _ => Err(Exception::IllegalInstruction(inst)),
+                }
+            }
+            0x17 => {
+                // auipc
+                let imm = (inst & 0xfffff000) as i32 as i64 as u64;
+                self.regs[rd] = self.pc.wrapping_add(imm);
+                return self.update_pc();
+            }
+            0x1b => {
+                let imm = ((inst as i32 as i64) >> 20) as u64;
+                // "SLLIW, SRLIW, and SRAIW encodings with imm[5] ̸= 0 are reserved."
+                let shamt = (imm & 0x1f) as u32;
+                match funct3 {
+                    0x0 => {
+                        // addiw
+                        self.regs[rd] = self.regs[rs1].wrapping_add(imm) as i32 as i64 as u64;
+                        return self.update_pc();
+                    }
+                    0x1 => {
+                        // slliw
+                        self.regs[rd] = self.regs[rs1].wrapping_shl(shamt) as i32 as i64 as u64;
+                        return self.update_pc();
+                    }
+                    0x5 => {
+                        match funct7 {
+                            0x00 => {
+                                // srliw
+                                self.regs[rd] = (self.regs[rs1] as u32).wrapping_shr(shamt) as i32
+                                    as i64 as u64;
+                                return self.update_pc();
+                            }
+                            0x20 => {
+                                // sraiw
+                                self.regs[rd] =
+                                    (self.regs[rs1] as i32).wrapping_shr(shamt) as i64 as u64;
+                                return self.update_pc();
+                            }
+                            _ => Err(Exception::IllegalInstruction(inst)),
+                        }
+                    }
+                    _ => Err(Exception::IllegalInstruction(inst)),
+                }
+            }
+            0x23 => {
+                // imm[11:5|4:0] = inst[31:25|11:7]
+                let imm = (((inst & 0xfe000000) as i32 as i64 >> 20) as u64) | ((inst >> 7) & 0x1f);
+                let addr = self.regs[rs1].wrapping_add(imm);
+                match funct3 {
+                    0x0 => {
+                        self.store(addr, 8, self.regs[rs2])?;
+                        self.update_pc()
+                    } // sb
+                    0x1 => {
+                        self.store(addr, 16, self.regs[rs2])?;
+                        self.update_pc()
+                    } // sh
+                    0x2 => {
+                        self.store(addr, 32, self.regs[rs2])?;
+                        self.update_pc()
+                    } // sw
+                    0x3 => {
+                        self.store(addr, 64, self.regs[rs2])?;
+                        self.update_pc()
+                    } // sd
+                    _ => unreachable!(),
+                }
+            }
+            0x33 => {
+                // "SLL, SRL, and SRA perform logical left, logical right, and arithmetic right
+                // shifts on the value in register rs1 by the shift amount held in register rs2.
+                // In RV64I, only the low 6 bits of rs2 are considered for the shift amount."
+                let shamt = ((self.regs[rs2] & 0x3f) as u64) as u32;
+                match (funct3, funct7) {
+                    (0x0, 0x00) => {
+                        // add
+                        self.regs[rd] = self.regs[rs1].wrapping_add(self.regs[rs2]);
+                        return self.update_pc();
+                    }
+                    (0x0, 0x01) => {
+                        // mul
+                        self.regs[rd] = self.regs[rs1].wrapping_mul(self.regs[rs2]);
+                        return self.update_pc();
+                    }
+                    (0x0, 0x20) => {
+                        // sub
+                        self.regs[rd] = self.regs[rs1].wrapping_sub(self.regs[rs2]);
+                        return self.update_pc();
+                    }
+                    (0x1, 0x00) => {
+                        // sll
+                        self.regs[rd] = self.regs[rs1].wrapping_shl(shamt);
+                        return self.update_pc();
+                    }
+                    (0x2, 0x00) => {
+                        // slt
+                        self.regs[rd] = if (self.regs[rs1] as i64) < (self.regs[rs2] as i64) { 1 } else { 0 };
+                        return self.update_pc();
+                    }
+                    (0x3, 0x00) => {
+                        // sltu
+                        self.regs[rd] = if self.regs[rs1] < self.regs[rs2] { 1 } else { 0 };
+                        return self.update_pc();
+                    }
+                    (0x4, 0x00) => {
+                        // xor
+                        self.regs[rd] = self.regs[rs1] ^ self.regs[rs2];
+                        return self.update_pc();
+                    }
+                    (0x5, 0x00) => {
+                        // srl
+                        self.regs[rd] = self.regs[rs1].wrapping_shr(shamt);
+                        return self.update_pc();
+                    }
+                    (0x5, 0x20) => {
+                        // sra
+                        self.regs[rd] = (self.regs[rs1] as i64).wrapping_shr(shamt) as u64;
+                        return self.update_pc();
+                    }
+                    (0x6, 0x00) => {
+                        // or
+                        self.regs[rd] = self.regs[rs1] | self.regs[rs2];
+                        return self.update_pc();
+                    }
+                    (0x7, 0x00) => {
+                        // and
+                        self.regs[rd] = self.regs[rs1] & self.regs[rs2];
+                        return self.update_pc();
+                    }
+                    _ => Err(Exception::IllegalInstruction(inst)),
+                }
+            }
+            0x37 => {
+                // lui
+                self.regs[rd] = (inst & 0xfffff000) as i32 as i64 as u64;
+                return self.update_pc();
+            }
+            0x3b => {
+                // "The shift amount is given by rs2[4:0]."
+                let shamt = (self.regs[rs2] & 0x1f) as u32;
+                match (funct3, funct7) {
+                    (0x0, 0x00) => {
+                        // addw
+                        self.regs[rd] =
+                            self.regs[rs1].wrapping_add(self.regs[rs2]) as i32 as i64 as u64;
+                        return self.update_pc();
+                    }
+                    (0x0, 0x20) => {
+                        // subw
+                        self.regs[rd] =
+                            ((self.regs[rs1].wrapping_sub(self.regs[rs2])) as i32) as u64;
+                        return self.update_pc();
+                    }
+                    (0x1, 0x00) => {
+                        // sllw
+                        self.regs[rd] = (self.regs[rs1] as u32).wrapping_shl(shamt) as i32 as u64;
+                        return self.update_pc();
+                    }
+                    (0x5, 0x00) => {
+                        // srlw
+                        self.regs[rd] = (self.regs[rs1] as u32).wrapping_shr(shamt) as i32 as u64;
+                        return self.update_pc();
+                    }
+                    (0x5, 0x20) => {
+                        // sraw
+                        self.regs[rd] = ((self.regs[rs1] as i32) >> (shamt as i32)) as u64;
+                        return self.update_pc();
+                    }
+                    _ => Err(Exception::IllegalInstruction(inst)),
+                }
+            }
+            0x63 => {
+                // imm[12|10:5|4:1|11] = inst[31|30:25|11:8|7]
+                let imm = (((inst & 0x80000000) as i32 as i64 >> 19) as u64)
+                    | ((inst & 0x80) << 4) // imm[11]
+                    | ((inst >> 20) & 0x7e0) // imm[10:5]
+                    | ((inst >> 7) & 0x1e); // imm[4:1]
+
+                match funct3 {
+                    0x0 => {
+                        // beq
+                        if self.regs[rs1] == self.regs[rs2] {
+                            return Ok(self.pc.wrapping_add(imm));
+                        }
+                        return self.update_pc();
+                    }
+                    0x1 => {
+                        // bne
+                        if self.regs[rs1] != self.regs[rs2] {
+                            return Ok(self.pc.wrapping_add(imm));
+                        }
+                        return self.update_pc();
+                    }
+                    0x4 => {
+                        // blt
+                        if (self.regs[rs1] as i64) < (self.regs[rs2] as i64) {
+                            return Ok(self.pc.wrapping_add(imm));
+                        }
+                        return self.update_pc();
+                    }
+                    0x5 => {
+                        // bge
+                        if (self.regs[rs1] as i64) >= (self.regs[rs2] as i64) {
+                            return Ok(self.pc.wrapping_add(imm));
+                        }
+                        return self.update_pc();
+                    }
+                    0x6 => {
+                        // bltu
+                        if self.regs[rs1] < self.regs[rs2] {
+                            return Ok(self.pc.wrapping_add(imm));
+                        }
+                        return self.update_pc();
+                    }
+                    0x7 => {
+                        // bgeu
+                        if self.regs[rs1] >= self.regs[rs2] {
+                            return Ok(self.pc.wrapping_add(imm));
+                        }
+                        return self.update_pc();
+                    }
+                    _ => Err(Exception::IllegalInstruction(inst)),
+                }
+            }
+            0x67 => {
+                // jalr
+                let t = self.pc + 4;
+
+                let imm = ((((inst & 0xfff00000) as i32) as i64) >> 20) as u64;
+                let new_pc = (self.regs[rs1].wrapping_add(imm)) & !1;
+
+                self.regs[rd] = t;
+                return Ok(new_pc);
+            }
+            0x6f => {
+                // jal
+                self.regs[rd] = self.pc + 4;
+
+                // imm[20|10:1|11|19:12] = inst[31|30:21|20|19:12]
+                let imm = (((inst & 0x80000000) as i32 as i64 >> 11) as u64) // imm[20]
+                    | (inst & 0xff000) // imm[19:12]
+                    | ((inst >> 9) & 0x800) // imm[11]
+                    | ((inst >> 20) & 0x7fe); // imm[10:1]
+
+                return Ok(self.pc.wrapping_add(imm));
+            }
+            _ => Err(Exception::IllegalInstruction(inst)),
+        }
+    }
+
+
+    pub fn dump_pc(&self) {
+        println!("{:-^80}", "PC register");
+        println!("PC = {:#x}\n", self.pc);
+    }
+
+    pub fn dump_registers(&mut self) {
+        println!("{:-^80}", "registers");
+        let mut output = String::new();
+        self.regs[0] = 0;
+
+        for i in (0..32).step_by(4) {
+            let i0 = format!("x{}", i);
+            let i1 = format!("x{}", i + 1);
+            let i2 = format!("x{}", i + 2);
+            let i3 = format!("x{}", i + 3);
+            let line = format!(
+                "{:3}({:^4}) = {:<#18x} {:3}({:^4}) = {:<#18x} {:3}({:^4}) = {:<#18x} {:3}({:^4}) = {:<#18x}\n",
+                i0, RVABI[i], self.regs[i],
+                i1, RVABI[i + 1], self.regs[i + 1],
+                i2, RVABI[i + 2], self.regs[i + 2],
+                i3, RVABI[i + 3], self.regs[i + 3],
+            );
+            output = output + &line;
+        }
+
+        println!("{}", output);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,44 @@
+mod bus;
+mod dram;
+mod exception;
+mod param;
+mod cpu;
+
 use std::{env, io};
 use std::fs::File;
 use std::io::Read;
+use crate::cpu::CPU;
 
-struct CPU {
-    // RISC-V 32 registers
-    regs: [u64; 32],
-    // pc register
-    pc: u64,
-    // memory
-    dram: Vec<u8>,
-}
-
-pub const DRAM_SIZE: u64 = 1024 * 1024 * 128;
-
-const RVABI: [&str; 32] = [
-    "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
-];
-
-impl CPU {
-    fn new(code: Vec<u8>) -> Self {
-        let mut regs = [0; 32];
-        regs[2] = DRAM_SIZE - 1;
-        Self {
-            regs,
-            pc: 0,
-            dram: code,
-        }
-    }
-    // fetch 32 bits instruction
-    fn fetch(&self) -> u32 {
-        let index = self.pc as usize;
-
-        let instr = self.dram[index] as u32
-            | ((self.dram[index + 1] as u32) << 8)
-            | ((self.dram[index + 2] as u32) << 16)
-            | ((self.dram[index + 3] as u32) << 24);
-        instr
-    }
-
-    fn execute(&mut self, instr: u32) {
-        // decode
-        let opcode = instr & 0b111_1111;
-        let rd = ((instr >> 7) & 0b1_1111) as usize;
-        let rs1 = ((instr >> 15) & 0b1_1111) as usize;
-        let rs2 = ((instr >> 20) & 0b1_1111) as usize;
-        let funct3 = (instr >> 12) & 0b111;
-        let funct7 = (instr >> 25) & 0b111_1111;
-        // x0 is hardwired zero
-        self.regs[0] = 0;
-        match opcode {
-            0x13 => {
-                // addi
-                let imm = ((instr & 0xfff0_0000) as i64 >> 20) as u64;
-                self.regs[rd] = self.regs[rs1].wrapping_add(imm);
-            }
-            0x33 => {
-                // add
-                self.regs[rd] = self.regs[rs1].wrapping_add(self.regs[rs2]);
-            }
-
-            _ => {
-                dbg!(format!("Invalid opcode: {:#x}", opcode));
-            }
-        }
-    }
-
-    pub fn dump_register(&mut self) {
-        println!("{:-^80}", "registers");
-        let mut output = String::new();
-        self.regs[0] = 0;
-        for i in (0..32).step_by(4) {
-            let i0 = format!("x{}", i);
-            let i1 = format!("x{}", i + 1);
-            let i2 = format!("x{}", i + 2);
-            let i3 = format!("x{}", i + 3);
-            let line = format!(
-                "{:3}({:^4}) = {:<#18x} {:3}({:^4}) = {:<#18x} {:3}({:^4}) = {:<#18x} {:3}({:^4}) = {:<#18x}\n",
-                i0, RVABI[i], self.regs[i],
-                i1, RVABI[i + 1], self.regs[i + 1],
-                i2, RVABI[i + 2], self.regs[i + 2],
-                i3, RVABI[i + 3], self.regs[i + 3],
-            );
-            output = output + &line;
-        }
-        println!("{}", output);
-    }
-}
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!(
-            "Usage:\n\
-            - cargo run <filename>"
-        );
-        return Ok(());
-    }
 
-    let mut file = File::open(&args[1])?;
-    let mut code = Vec::new();
-    file.read_to_end(&mut code)?;
-    let mut cpu = CPU::new(code);
-    while cpu.pc < cpu.dram.len() as u64 {
-        let inst = cpu.fetch();
-        cpu.execute(inst);
-        cpu.pc += 4;
+    if (args.len() != 2) && (args.len() != 3) {
+        panic!("Usage: rvemu-for-book <filename> <(option) image>");
     }
-    cpu.dump_register();
+    let mut file = File::open(&args[1])?;
+    let mut binary = Vec::new();
+    file.read_to_end(&mut binary)?;
+    let mut cpu = CPU::new(binary);
+    loop {
+        let instr = match cpu.fetch() {
+            Ok(instr) => instr,
+            Err(e) => {
+                println!("{}", e);
+                break;
+            }
+        };
+        match cpu.execute(instr) {
+            // Break the loop if an error occurs.
+            Ok(new_pc) => cpu.pc = new_pc,
+            Err(e) => {
+                println!("{}", e);
+                break;
+            }
+        };
+    }
+    cpu.dump_registers();
+    cpu.dump_pc();
+
     Ok(())
 }
